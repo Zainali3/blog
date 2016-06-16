91427a787232947cee9391c2e627a48b2699fb8a
Zain Ali <zain.ali@square63.com>
2016-06-16 14:50:06 +0500

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+def index
+	@posts=Post.all
+end
+def new
+	@post= Post.new
+end
+def create
+	@post = Post.new(params[:post])
+	if @post.save
+		redirect_to posts_path , :notice => "Your post is successfully saved"
+	else 
+		render "new"
+	end
+end
+def edit
+	@post= Post.find(params[:id])
+end
+def show
+	@post = Post.find(params[:id])
+end
+def update
+	@post = Post.find(params[:id])
+	if @post.update_attributes(params[:post])
+		redirect_to posts_path , :notice => "Your post is updated"
+	else 
+		render "edit"
+	end
+end
+def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+			redirect_to posts_path , :notice => "Your post is deleted"
+		
+end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0c614450cdd3043af4a67de5b7436c1dce6c69d7095dc4be5b0ff9f9e32454bc8e8589d2aa65f2123317bd36698d0dc01ce5c6cea3a087573b81dea53f86f458'
